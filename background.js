6c1abece03c228846c7d9fbeb44391c8b068561e
nsshah5 <nsshah5@ncsu.edu>
2021-09-19 18:33:16 -0400

    added libre translate code

---
+++ background.js
@@ -0,0 +1,11 @@
+const res = await fetch("https://libretranslate.com/translate", {
+	method: "POST",
+	body: JSON.stringify({
+		q: "",
+		source: "en",
+		target: "es"
+	}),
+	headers: { "Content-Type": "application/json" }
+});
+
+console.log(await res.json());
