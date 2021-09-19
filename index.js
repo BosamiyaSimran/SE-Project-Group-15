cea59d297f65a9af2632625e483ef213b35b9bee
Parth Thosani <thosaniparth0@gmail.com>
2021-09-19 18:25:45 -0400

    Added dropdown and js, html files

---
+++ index.js
@@ -0,0 +1,5 @@
+let selectedLanguage = document.getElementById("language");
+
+var valueofSelectedLanguage = selectedLanguage.options[selectedLanguage.selectedIndex].value;
+console.log(valueofSelectedLanguage);
+
