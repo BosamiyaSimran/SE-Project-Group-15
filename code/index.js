a83f62a82e004164d546a98f534a25f0a29c0e10
Parth Thosani <thosaniparth0@gmail.com>
2021-09-29 20:05:00 -0400

    Updated index.js after using Prettier

--- code/index.js
+++ code/index.js
@@ -1,52 +1,51 @@
 languageSelectDropdown = document.getElementById("language");
 
 selectedLanguage = languageSelectDropdown.value;
-languageSelectDropdown.addEventListener('change', ()=>{
-  selectedLanguage =languageSelectDropdown.value;
-  let data = {text: selectedText, toLanguage: selectedLanguage};
-  makeRequest('POST',"http://localhost:3000/translateText",data);
+languageSelectDropdown.addEventListener("change", () => {
+  selectedLanguage = languageSelectDropdown.value;
+  const data = { text: selectedText, toLanguage: selectedLanguage };
+  makeRequest("POST", "http://localhost:3000/translateText", data);
 });
 
-let selectedText = null
-chrome.tabs.executeScript( {
-  code: "window.getSelection().toString();"
-}, function(selection) {
-  document.getElementById("output").value = selection[0];
-  selectedText = selection[0];
- 
-  let data = {text: selectedText, toLanguage: selectedLanguage};
- 
-makeRequest('POST',"http://localhost:3000/translateText",data);
-});
+let selectedText = null;
+chrome.tabs.executeScript(
+  {
+    code: "window.getSelection().toString();",
+  },
+  function (selection) {
+    document.getElementById("output").value = selection[0];
+    selectedText = selection[0];
+
+    const data = { text: selectedText, toLanguage: selectedLanguage };
+
+    makeRequest("POST", "http://localhost:3000/translateText", data);
+  }
+);
 
 function makeRequest(method, url, data = null) {
   return new Promise(function (resolve, reject) {
-      let xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.send(JSON.stringify(data));
-      xhr.onload = function () {
-          if (this.status >= 200 && this.status < 300) {
-            let temp =  xhr.response
-           
-              resolve(xhr.response);
-            alert(selectedText + " in " + selectedLanguage + " is " + temp);
-          } else {
-           
-              reject({
-                  status: this.status,
-                  statusText: xhr.statusText
-              });
-          }
-      };
-      xhr.onerror = function () {
-          reject({
-              status: this.status,
-              statusText: xhr.statusText
-          });
-      };
-      
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(data));
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        const temp = xhr.response;
+
+        resolve(xhr.response);
+        alert(selectedText + " in " + selectedLanguage + " is " + temp);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText,
+      });
+    };
   });
 }
-
-
